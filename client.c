62b8a779988b11e4f79abb6001a4606f20cccd34
Nailouck <sergix.fomin@yandex.ru>
2024-11-22 20:40:16 +0300

    The virst version of the primitive messenger had created.

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -7,26 +8,42 @@
 #include <unistd.h>
 #include "err_proc.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+
+	if (argc < 2) {
+		puts("Not enough program arguments! Example: ./object <port>\n");
+		return -1;
+	}
+
 	int client_socket = Socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(9898);
+	addr.sin_port = htons(atoi(argv[1]));
 	addr.sin_addr.s_addr = INADDR_ANY;
 
 	Connect(client_socket, (struct sockaddr*)&addr, sizeof(addr));
-
-	write(client_socket, "Hello\n", 6);
-	char buff[256];
-	ssize_t nread = read(client_socket, buff, 256);
-	if (nread == -1) {
-		perror("Reading error!");
-		exit(EXIT_FAILURE);
+	char buff[512];
+	int disconnect = 0;
+	do {
+		if (disconnect == 3) { break; }
+		bzero(buff, 512);
+		fgets(buff, 512, stdin);
+		buff[strlen(buff) - 1] = '\n';
+		buff[strlen(buff)] = '\0';
+		write(client_socket, buff, strlen(buff) * sizeof(char));
+		ssize_t nread = read(client_socket, buff, 512);
+		if (nread == -1) {
+			perror("Reading error!");
+			exit(EXIT_FAILURE);
+		}
+		if (nread == 0) {
+			printf("Ñlient has disconnected.\n");
+			disconnect++;
+		}
+		write(STDOUT_FILENO, buff, nread);
 	}
-	if (nread == 0) {
-		printf("End of file.\n");
-	}
-	write(STDOUT_FILENO, buff, nread);
+	while (buff[0] + buff[1] + buff[2] != 'E' + '\n' + '\0');
+	
 	close(client_socket);
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "err_proc.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        puts("Not enough program arguments! Example: ./object <port>\n");
+        return -1;
+    }
 
     int server_socket = Socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(9898);
+    addr.sin_port = htons(atoi(argv[1]));
     addr.sin_addr.s_addr = INADDR_ANY;
 
     Bind(server_socket, (struct sockaddr*) &addr, sizeof(addr));
@@ -23,18 +29,29 @@ int main() {
     int client_socket = Accept(server_socket, (struct sockaddr*) &addr, &adrlen);
 
     ssize_t nread;
-    char buff[256];
-    nread = read(client_socket, buff, 256);
-    if (nread == -1) {
-        perror("Reading error!");
-        exit(EXIT_FAILURE);
-    }
-    if (nread == 0) {
-        printf("End of file.\n");
-    }
+    char buff[512];
+    int disconnect = 0;
+    do {
+        if (disconnect == 3) { break; }
+        bzero(buff, 512);
+        nread = read(client_socket, buff, 512);
+        if (nread == -1) {
+            perror("Reading error!");
+            exit(EXIT_FAILURE);
+        }
+        if (nread == 0) {
+            printf("Ñlient has disconnected.\n");
+            disconnect++;
+        }
 
-    write(STDOUT_FILENO, buff, nread);
-    write(client_socket, buff, nread);
+        write(STDOUT_FILENO, buff, nread);
+        bzero(buff, 512);
+        fgets(buff, 512, stdin);
+        buff[strlen(buff) - 1] = '\n';
+        buff[strlen(buff)] = '\0';
+        write(client_socket, buff, strlen(buff) * sizeof(char));
+    } while (buff[0] + buff[1] + buff[2] != 'E' + '\n' + '\0');
+    
 
     sleep(1);
     close(client_socket);
